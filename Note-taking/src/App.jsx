dbd033b8b6794cbb06ac52ae9065153558cbf373
DaffafauzanD <daffafauzand24@gmail.com>
2024-01-10 16:50:25 +0700

    push testing editor react

---
+++ Note-taking/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import Editor from "./Editor";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App">
+      <h1>Plain Text Example</h1>
+      <p>Note: this is an experimental build of Lexical</p>
+      <Editor />
+      <div className="other">
+        <h2>Other Examples</h2>
+        <ul>
+          <li>
+            <a
+              href="https://codesandbox.io/s/lexical-rich-text-example-5tncvy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Rich text example
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
