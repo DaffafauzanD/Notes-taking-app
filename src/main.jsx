a024b3cbf91b18c9d66dc7e76d451f8d78c6048e
macbookair <daffafauzand24@gmail.com>
2024-01-11 16:32:29 +0700

    Update main.jsx add editorjs instance

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './scss/style.scss'
 import * as bootstrap from 'bootstrap'
+import EditorContextProvider from './component/EditorContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <EditorContextProvider>
+      <App />
+    </EditorContextProvider>
   </React.StrictMode>,
 )
