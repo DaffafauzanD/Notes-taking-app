769d375a8337a349a69b36714954a277c8b10317
DaffafauzanD <daffafauzand24@gmail.com>
2024-01-12 00:07:23 +0700

    Update editorjs has max width and modify pop up button

--- src/component/ShowNote.jsx
+++ src/component/ShowNote.jsx
@@ -15,12 +15,11 @@ function ShowNote() {
 
     return (
         <>
-            <div className="col ps-md-2 pt-2">
-                <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" className="border rounded-3 p-1 text-decoration-none"><i className="bi bi-list bi-lg py-2 p-1"></i> Menu</a>
-                <div id="editorjs"></div>
-            </div>
+
             <div className="collapse" id="note">
-                
+                <div>
+                    <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded" id="editorjs"></div>
+                </div>
                 <div>
                     <button className="btn btn-primary">save</button>
                     <button className="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#note">Close</button>
--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -9,23 +9,17 @@ function SideBar() {
                     <div className="col-auto px-0">
                         <div id="sidebar" className="collapse collapse-horizontal show border-end">
                             <div id="sidebar-nav" className="list-group border-0 rounded-0 text-sm-start min-vh-100">
-                                <a href="#" className="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i className="bi bi-bootstrap"></i> <span>Item</span> </a>
-                                <a href="#" className="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i className="bi bi-film"></i> <span>Item</span></a>
-                                <a href="#" className="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i className="bi bi-heart"></i> <span>Item</span></a>
-                                <button className="btn btn-primary d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#note">
-                                    <span className="pe-2">New note</span>
-                                    <i className="bi bi-journal-plus fs-2"></i>
-                                </button>
-
-                                <button className="btn btn-primary d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#CardShow">
-                                    <span className="pe-2">Show note</span>
-                                    <i className="bi bi-journal-plus fs-2"></i>
-                                </button>
+                                <a href="#" className="list-group-item border-end-0 d-inline-block text-truncate" data-bs-toggle="collapse" data-bs-target="#note"><i className="bi bi-bootstrap"></i> <span>New Note</span> </a>
+                                <a href="#" className="list-group-item border-end-0 d-inline-block text-truncate" data-bs-toggle="collapse" data-bs-target="#CardShow"><i className="bi bi-film"></i> <span>Show Note</span></a>
+                                <a href="#" className="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar" ><i className="bi bi-heart"></i> <span>Item</span></a>
                             </div>
                         </div>
                     </div>
-                    <ShowNote />
-                    <EditModal />
+                    <div className="col ps-md-2 pt-2">
+                        <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" className="border rounded-3 p-1 text-decoration-none"><i className="bi bi-list bi-lg py-2 p-1"></i> Menu</a>
+                        <ShowNote />
+                        <EditModal />
+                    </div>
                 </div>
             </div>
         </>
